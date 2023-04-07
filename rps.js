91b99942b2b1a7c8e70bfb420f106f2d2a833789
Vineet829 <vineetsingh5987@gmail.com>
2023-04-07 06:51:19 +0530

    add UI

--- rps.js
+++ rps.js
@@ -11,50 +11,135 @@ function getComputerChoice(){
             return 'scissors'.toLowerCase();
         }
         }
-
-
-
-
-function round(){
-    playerSelection = prompt('Enter your choice').toLowerCase();
-    let computerSelection = getComputerChoice();
-    if(playerSelection == computerSelection){
-        return "Draw";
-    }
+        let result; 
+        let playerSelection;
+        let a=0;
+        let b=0;
+        let c=0;
+        let y;
+        function round(){
     
-       else if((playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'paper')){
-           return "Win";
-         }
-         else if((playerSelection == 'scissors' && computerSelection == 'rock') || (playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors')){
-            return "Lose";
-         }
-    }
-function game(){
-let a = 0, b = 0, c = 0;
-
-    for(let i =0; i<5; i++){
-    let result = round();
-    alert(result);    
-    if(result == "Win"){
-      a++;
-    }
-    else if (result == "Lose"){
-      b++;
-    }
-    else if(result == "Draw"){
-    c++;}
-}
-  if(a>b){
-    alert("Winner");
-} 
-else if(b>a){
-    alert("Loser");
+          let computerSelection = getComputerChoice();
+          if(playerSelection == computerSelection){
+              return "Draw";
+          }
+          
+             else if((playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'paper')){
+                 return "Win";
+               }
+               else if((playerSelection == 'scissors' && computerSelection == 'rock') || (playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors')){
+                  return "Lose";
+               }
+          
+        };
+function removescore(){
+  const score = document.querySelector(".score")
+  let q = score.childElementCount;
+  alert(y);
+  while(q!=0)   
+      score.removeChild(score.firstElementChild);
     
-}
-  else if(a === b){
-
-    alert("It's a Draw");}
-}
-  
+    } 
+    
  
   
+     
+  
+
+        const game =   function (){
+          const z = document.querySelector(".score");
+          let count = z.childElementCount;
+          if(count <5){
+                  
+              if(result == "Win"){
+                let score = document.querySelector(".score")
+                let txt = document.createElement('p');
+                
+                txt.textContent = "Win";
+                score.appendChild(txt); 
+              a++;  
+              }
+              else if (result == "Lose"){
+                let score = document.querySelector(".score")
+            let txt = document.createElement('p');
+            
+            txt.textContent = "Lose";
+            score.appendChild(txt); 
+                b++;
+              }
+              else if(result == "Draw"){
+                const score = document.querySelector(".score")
+                const txt = document.createElement('p');
+                
+                txt.textContent = "Draw";
+                score.appendChild(txt); 
+              c++;  
+              }}
+          
+              
+      else if(count =5) {
+       if(a>b){
+  const score = document.querySelector(".score")
+  const end = document.createElement('p')
+  end.textContent = "Winner";
+  score.appendChild(end)
+  y = "Winner";
+  removescore();
+  
+  
+  
+  }
+      else if(a<b){
+        const score = document.querySelector(".score")
+        const end = document.createElement('p')
+        end.textContent = "Loser";
+        y = "Loser";
+        score.appendChild(end) 
+    removescore();
+     
+      }
+  else if(a == b){
+    const score = document.querySelector(".score")
+    const end = document.createElement('p')
+    end.textContent = "Stalemate";
+    y = "Stalemate";
+    score.appendChild(end); 
+     removescore();
+     
+  }               
+ 
+}     
+  }   
+    
+        
+        const button1 = document.querySelector(".paper");
+        button1.addEventListener('click', () => {
+          playerSelection = "paper";
+          result = round(); 
+         game();
+          
+      
+      });
+        
+        const button2 = document.querySelector(".rock");
+        button2.addEventListener('click', () => {
+          playerSelection = "rock";
+          result = round(); 
+       game();});
+  
+        const button3 = document.querySelector(".scissors");
+        button3.addEventListener('click', () => {
+          playerSelection = "scissors";
+          result = round(); 
+        game();});
+        
+
+
+
+
+
+       
+
+
+
+
